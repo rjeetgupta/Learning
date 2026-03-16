64b50f3b8cc4eed6b48d356c4a1adf848bdb57d7
Ranjeet <ranjeeturd@gmail.com>
2026-03-16 13:21:58 +0530

    change message in client

--- Backend/UDPSERVER/client.js
+++ Backend/UDPSERVER/client.js
@@ -8,4 +8,4 @@ socket.on("message", (message, remoteAddress) => {
     console.log(remoteAddress);
 })
 
-socket.send("Hi kaise ho", 3000, "192.168.29.168")
+socket.send("Hi kaise ho, If messaged deliver then confirm me", 3000, "192.168.29.168")
