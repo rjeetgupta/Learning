d24735b291c202743ee137c84062489c6d35ad90
Ranjeet <ranjeeturd@gmail.com>
2026-06-27 13:00:00 +0530

    Add initial project files for site banner application
    - Create .gitignore to exclude logs, coverage, and build files.
    - Add bun.lock for dependency management.
    - Create package.json with project metadata and dependencies (express, ioredis, mongoose).
    - Implement src/index.js with Express server and Redis integration for managing banner messages.

---
+++ redis/03_site_banner/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Redis from "ioredis";
+
+const app = express();
+app.use(express.json());
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+const BANNER_KEY = "app:banner"
+
+app.post("/banner", async (req, res) => {
+    await redis.set(BANNER_KEY, req.body.message || "Welcome to my website");
+    res.json({ success: true });
+});
+
+app.get("/banner", async (req, res) => {
+    const message = await redis.get(BANNER_KEY);
+    res.json({ message: message });
+});
+
+app.delete("/banner", async (req, res) => {
+    await redis.del(BANNER_KEY);
+    res.json({ success: true });
+});
+
+app.get("/banner/exist", async (req, res) => {
+    const exist = await redis.exists(BANNER_KEY);
+    res.json({ exist: Boolean(exist) });
+
+});
+
+app.listen(3000, (req, res) => {
+    console.log(`Application is listening on http://localhost:3000`);
+})
