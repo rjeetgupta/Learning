1c752f3351d9f88bf4459f3db836c4a4ccd4aeab
Ranjeet <ranjeeturd@gmail.com>
2026-07-07 13:18:20 +0530

    feat: add initial implementation of simple values demonstration in Go

---
+++ golang/02_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	fmt.Println(1)
+
+	// int
+	fmt.Println(2)
+
+	// string
+	fmt.Println("Hello from Go lang")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(24.2)
+	fmt.Println(24.0 / 3.0)
+}
